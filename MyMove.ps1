20654a23ff8721f400b29a83182045527c5c13d5
Tim <44636626+TimPeTwo@users.noreply.github.com>
2024-09-21 13:12:42 +0200

    Update MyMove.ps1
    Kommentar verbessert

--- MyMove.ps1
+++ MyMove.ps1
@@ -4,14 +4,17 @@ $ErrorActionPreference = "stop"
 $gmapsupp = "gmapsupp.img"
 $gmapbmap = "gmapbmap.img"
 
-
+# Rename the file
 if ( (Test-Path -Path $gmapsupp)) {
     Move-Item $gmapsupp $gmapbmap -Force -Verbose
 }
 
+# Throw a error, if file does not exist
 if (-not ( test-path -path $gmapbmap )) {
     throw "Datei gmapbmap.img nicht vorhanden."
 }
+
+# The device is not allway on the same drive.
 $Pathes = "d:\Garmin", "e:\Garmin", "g:\Garmin"
 foreach ($Path in $Pathes) {
     if (Test-Path $Path) {
